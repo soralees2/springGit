afb57240b2f026e57b4cc7aea65d38f6aa7456d1
eelsoras2 <eelsoras2@gmail.com>
2021-07-23 14:33:02 +0900

    로그인/로그아웃 완료

--- src/main/java/kh/spring/dao/MemberDAO.java
+++ src/main/java/kh/spring/dao/MemberDAO.java
@@ -1,8 +1,18 @@
 package kh.spring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDAO {
+	
+	@Autowired
+	private JdbcTemplate jdbc;
+	
+	public int login(String id,String pw) {
+		String sql = "select count(*) from member where id=? and pw=?";
+		return jdbc.queryForObject(sql, Integer.class,id,pw);
+	}
 
 }
